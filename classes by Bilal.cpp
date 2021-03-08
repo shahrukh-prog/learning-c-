d4864b90fa5cc4d68765a423aaabd542f999403c
shahrukh-prog <79189810+shahrukh-prog@users.noreply.github.com>
2021-03-08 18:06:57 +0500

    Add files via upload

---
+++ classes by Bilal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class student 
+
+{
+	private;
+	
+	int roll;
+	float marks;
+	string name;
+	char grade;
+	
+	public:
+		student (int roll, float marks, string name, char grade)
+	{
+		setroll(this->roll);
+		setfloat(this->marks);
+		setstring(this->name);
+		setgrade(this-grade>);
+	}
+	
+	public:
+		student ()
+	{
+		setroll(this->0);
+		setfloat(this->0.0);
+		setstring(this->"");
+		setgrade(this->'');
+	}
+	
+	public:
+		
+}
+
+int main ()
+
+{
+	
+}
